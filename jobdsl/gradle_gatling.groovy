48652fc3adec656c30528a2f343e208acee4985b
Tim Webster <tim.webster@gmail.com>
2018-06-16 14:36:46 +0100

    try some stuff

--- jobdsl/gradle_gatling.groovy
+++ jobdsl/gradle_gatling.groovy
@@ -1,18 +1,26 @@
 pipelineJob("gradle-gatling") {
-   triggers {
-      githubPush()
-   }
-   definition {
-       cpsScm {
-           scm {
-               git {
-                   remote {
-                       url('https://github.com/timwebster9/gradle-gatling.git')
-                   }
-                   //branch('master')
-               }
-           }
-           scriptPath('Jenkinsfile')
-       }
-   }
+
+    displayname('$GIT_LOCAL_BRANCH')
+
+    triggers {
+       githubPush()
+    }
+    
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    extensions {
+                        wipeOutWorkspace()
+                        localBranch()
+                    }
+                    remote {
+                        url('https://github.com/timwebster9/gradle-gatling.git')
+                    }
+                    //branch('master')
+                }
+            }
+            scriptPath('Jenkinsfile')
+        }
+    }
 }
